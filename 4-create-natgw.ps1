462cc50af20ddcd723b8393bdf04bc490c8c5144
infotechtutorials <infotechnologytutorials@gmail.com>
2026-03-22 11:01:34 -0400

    Update 4-create-natgw.ps1

--- 4-create-natgw.ps1
+++ 4-create-natgw.ps1
@@ -1,33 +1,33 @@
 # Variables
 
-$RESOURCE_GROUP="---------"
-$LOCATION="canadacentral"
-$VNET_NAME="---------"
-$SUBNET_NAME="---------"
-$NAT_NAME="------------"
-$PUBLIC_IP_NAME="-------------"
+$resourceGroup="---------"
+$location="----------"
+$vnetName="---------"
+$subnetName="---------"
+$natName="------------"
+$public-ip-name="-------------"
 
 # Create Public IP
 
 az network public-ip create `
-  --resource-group $RESOURCE_GROUP `
-  --name $PUBLIC_IP_NAME `
+  --resource-group $resourceGroup `
+  --name $public-ip-name `
   --sku Standard `
   --allocation-method Static `
-  --location $LOCATION
+  --location $location
 
 # Create NAT Gateway using the Public IP
 
 az network nat gateway create `
-  --resource-group $RESOURCE_GROUP `
-  --name $NAT_NAME `
-  --location $LOCATION `
-  --public-ip-addresses $PUBLIC_IP_NAME `
+  --resource-group $resourceGroup `
+  --name $natName `
+  --location $location `
+  --public-ip-addresses $public-ip-name `
   --idle-timeout 10
 
 # Attach NAT Gateway to subnet
 az network vnet subnet update `
-  --resource-group $RESOURCE_GROUP `
-  --vnet-name $VNET_NAME `
-  --name $SUBNET_NAME `
-  --nat-gateway $NAT_NAME
+  --resource-group $resourceGroup `
+  --vnet-name $vnetName `
+  --name $subnetName `
+  --nat-gateway $natName
