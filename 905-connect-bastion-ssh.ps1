63e7426a8b1811f84be4338983e146f5f13493a6
infotechtutorials <infotechnologytutorials@gmail.com>
2026-03-22 12:20:36 -0400

    Update 905-connect-bastion-ssh.ps1

--- 905-connect-bastion-ssh.ps1
+++ 905-connect-bastion-ssh.ps1
@@ -1,1 +1,1 @@
-ssh -o IdentitiesOnly=yes -i "C:\Users\raphael\.ssh\*ssh-key*" *username*@127.0.0.1 -p 50022
+ssh -o IdentitiesOnly=yes -i "C:\Users\*username*\.ssh\*ssh-key*" *username*@127.0.0.1 -p 50022
