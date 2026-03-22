b3b5f3e7eea9abaca7a3ec16b3209e826f9f4e34
infotechtutorials <infotechnologytutorials@gmail.com>
2026-03-21 20:48:10 -0400

    Update create-vnet.ps1

--- create-vnet.ps1
+++ create-vnet.ps1
@@ -1,23 +1,27 @@
 # Variables
 
-$ResourceGroup = "-----"
-$Location = "-----"
-$VNetName = "------"
-$AddressSpace = "--------"
-$SubnetName = "-------"
-$SubnetPrefix = "------"
+$ResourceGroup = "RG-web-prod"
+$Location = "CanadaCentral"
+$VNetName = "vnet-web-prod"
+$SubnetName = "subnet-web-prod"
+
+#  Addressing
+$VNetAddressSpace = "192.168.0.0/16"
+$SubnetPrefix     = "192.168.100.0/24"
 
 # Create Subnet configuration
 
 $Subnet = New-AzVirtualNetworkSubnetConfig `
-    -name $SubnetName `
-    -AddressPrefix $SubnetPrefix
+    -Name $SubnetName `
+    -AddressPrefix $SubnetPrefix `
+    -DefaultOutboundAccess $false
+
 
 # Create Virtual Network
 
 New-AzVirtualNetwork `
-     -ResourceGroupName $ResourceGroup `
-     -Location $Location `
-     -Name $VNetName `
-     -AddressPrefix $AddressSpace `
-     -Subnet $Subnet
+    -ResourceGroupName $ResourceGroup `
+    -Location $Location `
+    -Name $VNetName `
+    -AddressPrefix $VNetAddressSpace `
+    -Subnet $Subnet
